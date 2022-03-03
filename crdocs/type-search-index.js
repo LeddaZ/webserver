f910694d6a8515cf0eaae201b41c79d414018731
LeddaZ <leonardoledda@gmail.com>
2022-03-03 22:51:01 +0100

    Upload ClashRoyaleEmotes docs

---
+++ crdocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ClashRoyaleWindow"},{"p":"<Unnamed>","l":"ClashRoyaleWindowViewer"},{"p":"<Unnamed>","l":"EmoteButtonListener"}];updateSearchResults();
