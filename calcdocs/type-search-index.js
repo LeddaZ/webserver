427938033bb010b64d64450e5f5d523d48fd2cb6
LeddaZ <leonardoledda@gmail.com>
2022-03-16 22:56:00 +0100

    Add documentation for https://github.com/LeddaZ/Calculator

---
+++ calcdocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CalculatorListener"},{"p":"<Unnamed>","l":"CalculatorWindow"},{"p":"<Unnamed>","l":"ErrorDialog"},{"p":"<Unnamed>","l":"InvalidOperationException"}];updateSearchResults();
